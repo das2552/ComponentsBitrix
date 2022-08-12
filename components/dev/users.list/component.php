a9d8204477bac866cb852d0dee07aae9a9d2e409
Леша <aleksdementev4@gmail.com>
2022-08-12 15:23:02 +0300

    delete cache users.list
    кеш у листа пока не доделал, пока что вырезал для функционирования

--- components/dev/users.list/component.php
+++ components/dev/users.list/component.php
@@ -10,64 +10,51 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 /** @global CUser $USER */
 /** @global CMain $APPLICATION */
 
-// время кеширования
-if (!isset($arParams['CACHE_TIME'])) {
-    $arParams['CACHE_TIME'] = 3600;
-} else {
-    $arParams['CACHE_TIME'] = intval($arParams['CACHE_TIME']);
+
+
+$arUsers = CUser::GetList(($by="id"), ($order="asc")); // выбираем пользователей
+$is_filtered = $arUsers->is_filtered; // отфильтрована ли выборка
+$arUsers->NavStart(25); // разбиваем постранично по 25 записей
+$arResult["NAV"] = $arUsers->NavPrint(GetMessage("PAGES")); // постраничная навигация
+$arAllUsers = Array();
+$arOrders = Array();
+
+$rsSales = CSaleOrder::GetList(array("DATE_INSERT" => "ASC"));
+while ($arSales = $rsSales->Fetch())
+{
+	$arOrder["ID"] = $arSales["ID"];
+	$arOrder["USER_ID"] = $arSales["USER_ID"];
+	array_push($arOrders , $arOrder);
 }
 
-if ($this->StartResultCache()) {
+// прозодимся по каждому пользователю
+while($user=$arUsers->GetNext()){
+	$arUser = Array();
 	
-	$arUsers = CUser::GetList(($by="id"), ($order="asc")); // выбираем пользователей
-	$is_filtered = $arUsers->is_filtered; // отфильтрована ли выборка
-	$arUsers->NavStart(25); // разбиваем постранично по 25 записей
-	$arResult["NAV"] = $arUsers->NavPrint(GetMessage("PAGES")); // постраничная навигация
-	$arAllUsers = Array();
-	$arOrders = Array();
-	
-	$rsSales = CSaleOrder::GetList(array("DATE_INSERT" => "ASC"));
-	while ($arSales = $rsSales->Fetch())
-	{
-		$arOrder["ID"] = $arSales["ID"];
-		$arOrder["USER_ID"] = $arSales["USER_ID"];
-		array_push($arOrders , $arOrder);
-	}
-	
-	// прозодимся по каждому пользователю
-	while($user=$arUsers->GetNext()){
-		$arUser = Array();
-		
-		//счетчик заказов
-		$countOrders=0;
-		foreach($arOrders as $order) {
-			if($order["USER_ID"] == $user["ID"]){
-				$countOrders++;
-			}
+	//счетчик заказов
+	$countOrders=0;
+	foreach($arOrders as $order) {
+		if($order["USER_ID"] == $user["ID"]){
+			$countOrders++;
 		}
-		//формируем массив
-		$arUser["ID"] = $user["ID"];
-		$arUser["ACTIVE"] = $user["ACTIVE"];
-		$arUser["LOGIN"] = $user["LOGIN"];
-		$arUser["PERSONAL_PHOTO"] = $user["PERSONAL_PHOTO"];
-		$arUser["COUNT_ORDERS"] = $countOrders;
-		$arUser["DETAIL_PAGE_URL"] = "/users/?ELEMENT_ID=" . $user["ID"];
-
-		array_push($arAllUsers, $arUser);
 	}
-	
-	$arResult["ITEMS"] = $arAllUsers;
+	//формируем массив
+	$arUser["ID"] = $user["ID"];
+	$arUser["ACTIVE"] = $user["ACTIVE"];
+	$arUser["LOGIN"] = $user["LOGIN"];
+	$arUser["PERSONAL_PHOTO"] = $user["PERSONAL_PHOTO"];
+	$arUser["COUNT_ORDERS"] = $countOrders;
+	$arUser["DETAIL_PAGE_URL"] = "/users/?ELEMENT_ID=" . $user["ID"];
 
-	$this->SetResultCacheKeys(
-		array(
-			'ID',
-			'NAME'
-		)
-	);
-	// подключаем шаблон и сохраняем кеш
-	$this->IncludeComponentTemplate();
-
+	array_push($arAllUsers, $arUser);
 }
+
+$arResult["ITEMS"] = $arAllUsers;
+
+// подключаем шаблон и сохраняем кеш
+$this->IncludeComponentTemplate();
+
+
 
 
 
